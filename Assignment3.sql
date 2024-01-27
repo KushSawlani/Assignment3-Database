4a13145e3379fd24e8beb2005044941515e3e2a7
KUSH SAWLANI <124054246+KushSawlani@users.noreply.github.com>
2024-01-27 01:17:34 -0800

    Update Assignment3.sql

--- Assignment3.sql
+++ Assignment3.sql
@@ -3,9 +3,9 @@
 --users table
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    phone_number VARCHAR(20) NOT NULL
+    name VARCHAR(20) NOT NULL,
+    email VARCHAR(20) UNIQUE NOT NULL,
+    phone_number VARCHAR(10) NOT NULL
 );
 
 --tweets table
